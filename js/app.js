2d99f48b4562dc460a1f93a7ce1caf72fa879591
raphael <moudens.raphael@orange.fr>
2017-02-14 16:50:16 +0100

    ajout score et finition

--- js/app.js
+++ js/app.js
@@ -1,11 +1,18 @@
 var gameStart = false;
 var gameEnd = false;
 var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'jeu', { preload: preload, create: create, update: update });
+var score = 0;
+var scoreText;
+function collectStar (player, star) {
+	star.kill();
+	score += 10;
+	scoreText.text = 'Score:' + score;
+}
 //game.transparent = true;
 function preload() {
 	game.load.image('sky', 'assets/sky.png');
 	game.load.image('ground', 'assets/platform.png');
-	game.load.image('star', 'assets/star.png');
+	game.load.image('diamond', 'assets/diamond.png');
 	game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 
 }
@@ -28,12 +35,48 @@ function create(){
 	player.body.bounce.y = 0.2;
 	player.body.gravity.y = 300;
 	player.body.collideWorldBounds = true;
-	player.animations.add('left', [0, 1, 2, 3], 10, true);
+	player.animations.add('left', [0, 1, 3, 4], 10, true);
 	player.animations.add('right', [5, 6, 7, 8], 10, true);
-	//game.add.sprite(0, 0, 'star');
-	
+	stars = game.add.group();
+	scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+
+	stars.enableBody = true;
+
+	for (var i =0; i < 12; i++)
+	{
+		var star = stars.create(i * 70, 0, 'diamond');
+		star.body.gravity.y = 6;
+		star.body.bounce.y = 0.7 + Math.random() * 0.2;
+
+	}	
 }
 function update(){
+	cursors = game.input.keyboard.createCursorKeys();
 	var hitPlatform = game.physics.arcade.collide(player, platforms);
+	game.physics.arcade.collide(stars, platforms);
+	game.physics.arcade.overlap(player, stars, collectStar, null, this);
+	
+	player.body.velocity.x = 0;
+	if (cursors.left.isDown)
+	{
+		player.body.velocity.x = -150;
+		player.animations.play('left');
+
+	}
+	else if (cursors.right.isDown)
+	{
+		player.body.velocity.x = 150;
+		player.animations.play('right');
+	}	
+	else
+	{
+		player.animations.stop();
+		player.frame = 4;
+	}
+	if (cursors.up.isDown && player.body.touching.down && hitPlatform)
+	{
+		player.body.velocity.y = -350;
+	}
 
 }
+
